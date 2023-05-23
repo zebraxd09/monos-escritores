3e1df1823e237e695b900b567cbabbe018cd669f
Luis D <chirimoyaluis@protonmail.ch>
2023-05-22 22:34:39 -0600

    [FEAT] texto de input ahora aparece color rojo

--- monos.php
+++ monos.php
@@ -9,17 +9,19 @@
 
 <body align="center" style="background-color: #d7d7d2;">
     <?php
-        // $i=(isset($_POST['valor']) && $_POST["valor"] != "")? $_POST['valor'] : "Falta Valor";   
+        // $t_mode=(isset($_POST['t-mode']) && $_POST["t-mode"] != "")? $_POST['t-mode'] : "Falta Valor";
+        // $word_s=(isset($_POST['t_word-s']) && $_POST["t_word-s"] != "")? $_POST['t_word-s'] : "Falta Valor";
+        // $time_zone=(isset($_POST['time-zone']) && $_POST["time-zone"] != "")? $_POST['time-zone'] : "Falta Valor";
         $t_mode=$_POST['t-mode'];
         $word_s=$_POST['word-s'];
         $time_zone=$_POST['time-zone'];
         $word_s=explode(" ", $word_s);
-        $length_text=251-count($word_s);        
+        $length_text=251-count($word_s);     
         echo '<table align="center" border="1" style="border-collapse:collapse; " cellpadding="30px">';
             echo '<thead>';
                 echo '<tr>';
                     echo '<th><strong>Libro número: ';
-                    for($i=1;$i<=10;$i++) 
+                        for($i=1;$i<=10;$i++) 
                         { 
                             $mayus=rand(65,90);
                             $minus=rand(98,122);
@@ -46,32 +48,60 @@
                         $i_words=array();
                         $all_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
                         while(count($i_words)<$length_text) 
-                        {
+                        {    
                             $length_word=rand(1,10);
-                            $word='';
+                            $t_word='';
                             for($i=0;$i<$length_word;$i++) 
                             {
                                 $rand_i=rand(0,strlen($all_chars)-1);
                                 $rand_char=$all_chars[$rand_i];
-                                $word.=$rand_char;
+                                $t_word.=$rand_char;
                             }
-                            $i_words[]=$word;                            
-                        }                    
-                        $text=implode(" ",$i_words);
-                        echo $text;    
+                            $i_words[]=$t_word;
+                        }
+                        // $text=implode(" ",$i_words);
+                        // var_dump($i_words);
                         switch($t_mode)
                         {
                             case'tm-normal':
-                                // normal();
+                                $unidos=implode(" ", $word_s);
+                                array_push($i_words, $unidos);
+                                shuffle($i_words);
+                                foreach($i_words as $valor)
+                                {
+                                    if($valor==$unidos)
+                                    {
+                                        echo '<strong style="color:red";>'.$valor." ".'</strong>';
+                                    }
+                                    else
+                                    {
+                                        echo $valor." ";
+                                    }
+                                }
                                 break;
-                            case'tm-words':{
-                                foreach($word_s as $valor){                                                                                                                
-                                        echo "<strong style='color:red';>$valor</strong>";                                                                            
-                                        echo $text;
-                                        }
+                            case'tm-words':
+                                foreach($word_s as $valor)
+                                {                                                                                                                
+                                        echo '<strong style="color:red";>'." ".$valor." ".'</strong>';
+                                        echo $text;     
                                 }                             
                                 break;
-                            case'tm-order':
+                            case'tm-disorder':
+                                shuffle($word_s);
+                                $unidos=implode(" ", $word_s);
+                                array_push($i_words, $unidos);
+                                shuffle($i_words);
+                                foreach($i_words as $valor)
+                                {
+                                    if($valor==$unidos)
+                                    {
+                                        echo '<strong style="color:red";>'.$valor." ".'</strong>';
+                                    }
+                                    else
+                                    {
+                                        echo $valor." ";
+                                    }
+                                }
                                 break;
                         }
                     echo '</td>';
